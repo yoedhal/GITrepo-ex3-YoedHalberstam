87aa457dc052a2515c7aee984f07d40e7dd9b5b2
Yoed Halberstam <yoedha@mta.ac.il>
2026-04-20 14:02:30 +0300

    Fix YoedHalberstam class name

--- YoedHalberstam.java
+++ YoedHalberstam.java
@@ -1,4 +1,4 @@
-public class Hello{
+public class YoedHalberstam{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
             System.out.println("YoedHalberstam "+i);
