b686dfcc23b8a3819c2fe2ad23022a6914496233
Yoed Halberstam <yoedha@mta.ac.il>
2026-04-20 14:05:47 +0300

    Update YoedHalberstam print

--- YoedHalberstam.java
+++ YoedHalberstam.java
@@ -1,7 +1,7 @@
 public class YoedHalberstam{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("YoedHalberstam "+i);
+            System.out.println("Yoed"+i);
         }
     }
 }
